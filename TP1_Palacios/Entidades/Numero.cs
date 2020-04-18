7ed9a82d340f8383658973018b3296bba72f4c14
NahueP <nahuelpalacios@hotmail.com>
2020-04-18 19:53:54 -0300

    TP1_Palacios

---
+++ TP1_Palacios/Entidades/Numero.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        #region Atributos
+        private double numero;
+        #endregion
+
+        #region Propiedades
+        public string SetNumero
+        {
+            set { this.numero = ValidarNumero(value); }
+        }
+        #endregion
+
+        #region Constructores
+        public Numero() : this(0)
+        {
+
+        }
+
+        public Numero(double numero) : this(numero.ToString())
+        {
+
+        }
+
+        public Numero(string strNumero)
+        {
+            SetNumero = strNumero;
+        }
+        #endregion
+
+        #region Metodos y Sobrecargas
+
+        private double ValidarNumero(string strNumero)
+        {
+            double retorno = 0;
+
+            double.TryParse(strNumero, out retorno);
+
+            return retorno;
+
+        }
+        
+
+        public static double operator +(Numero n1, Numero n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        public static double operator *(Numero n1, Numero n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        public static double operator /(Numero n1, Numero n2)
+        {
+            return n1.numero / n2.numero;
+        }
+
+
+        public string BinarioDecimal(string binario)
+        {
+            string retorno = "valor invalido";
+
+            bool valido = true;
+
+            foreach (var c in binario)
+            {
+                if (c != '0' && c != '1')
+                {
+                    valido = false;
+                }
+            }
+            if (valido == true)
+            {
+                retorno = Convert.ToInt32(binario, 2).ToString();
+            }
+            return retorno;
+        }
+
+        
+        public string DecimalBinario(double numero)
+        {
+            return DecimalBinario(numero.ToString());
+        }
+
+        
+        public string DecimalBinario(string numero)
+        {
+            string retorno = "valor invalido";
+
+            decimal value;
+
+            uint value2;
+
+            if (Decimal.TryParse(numero, out value))
+            {
+                if (UInt32.TryParse(numero, out value2))
+                { 
+                    retorno = Convert.ToString(Convert.ToInt32(numero, 10), 2);
+                }
+            }
+
+            return retorno;
+        }
+
+        #endregion
+
+    }
+}
---
+++ TP1_Palacios/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+
+            this.lblResultado.Text = "";
+
+            this.cmbOperador.Items.Add("+"); 
+            this.cmbOperador.Items.Add("-");
+            this.cmbOperador.Items.Add("*");
+            this.cmbOperador.Items.Add("/");
+
+            btnConvertirABinario.Enabled = false;
+            btnConvertirADecimal.Enabled = false;
+        }
+
+        #region Metodos
+        private void Limpiar()
+        {
+            this.txtNumero1.Text = "";
+            this.txtNumero2.Text = "";
+            this.lblResultado.Text = "";
+            this.cmbOperador.SelectedIndex = -1;
+            btnConvertirABinario.Enabled = false;
+            btnConvertirADecimal.Enabled = false;
+        }
+
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            Numero num1 = new Numero(numero1);
+            Numero num2 = new Numero(numero2);
+
+            return Calculadora.Operar(num1, num2, operador);
+        }
+        #endregion
+
+        #region Eventos
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+       
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            this.lblResultado.Text = (Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text)).ToString();
+            btnConvertirABinario.Enabled = true;
+            btnConvertirADecimal.Enabled = false;
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            Numero aBinario = new Numero();
+            lblResultado.Text = aBinario.DecimalBinario(lblResultado.Text);
+
+            btnConvertirABinario.Enabled = false;
+            btnConvertirADecimal.Enabled = true;
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            Numero aDecimal = new Numero();
+            lblResultado.Text = aDecimal.BinarioDecimal(lblResultado.Text);
+
+            btnConvertirADecimal.Enabled = false;
+            btnConvertirABinario.Enabled = true;
+        }
+
+        #endregion
+    }
+}
